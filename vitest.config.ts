89fed22fb4ab579a86c0accb4f0515efcc9acc5b
Adrian Orłów <adrian@orlow.me>
2025-11-20 00:07:37 +0100

    Initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    include: ["**/*.test.tsx", "**/*.test.ts"],
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+  },
+});
