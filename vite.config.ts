356e4aecf630a7451b1e76408bbe51d64c7ca427
Adrian Orłów <adrian@orlow.me>
2025-11-20 00:30:29 +0100

    chore: make project a library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    dts({
+      include: ["lib"],
+      tsconfigPath: "./tsconfig.app.json",
+      rollupTypes: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./lib"),
+    },
+  },
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+  },
 });
