25f49317910697054e8da52dd7c12eda25b2ed54
whit33y <troppout@gmail.com>
2026-01-28 21:10:26 +0100

    feat: add colors and necessary svgs to assets

--- src/components/ui/InfoMessage/InfoMessage.tsx
+++ src/components/ui/InfoMessage/InfoMessage.tsx
@@ -3,14 +3,14 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-const infoMessageVariants = cva("text-xs font-bold leading-[1.2]", {
+const infoMessageVariants = cva("text-xs font-bold leading-[1.2] align-middle", {
   variants: {
     variant: {
-      default: "",
-      info: "",
-      error: "",
-      warning: "",
-      success: "",
+      default: "text-gi-dark-gray",
+      info: "text-gi-blue",
+      error: "text-gi-red",
+      warning: "text-gi-orange",
+      success: "text-gi-green",
     },
   },
   defaultVariants: {
