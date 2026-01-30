7af96c45812d239ee83c441ae2b19dc117a3372d
whit33y <troppout@gmail.com>
2026-01-30 19:48:09 +0100

    feat: add svgr and icons to info-message component

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
+    svgr({
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
     dts({
       include: ["src"],
       tsconfigPath: "./tsconfig.app.json",
