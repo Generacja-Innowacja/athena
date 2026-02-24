4c8dd61eece4e5ca95cac73eab1d188c63fece76
kuba kesler <keslerkuba12@gmail.com>
2026-02-24 14:53:27 +0100

    Update TextInput.stories.tsx
    Signed-off-by: kuba kesler <keslerkuba12@gmail.com>

--- src/components/ui/TextInput/TextInput.stories.tsx
+++ src/components/ui/TextInput/TextInput.stories.tsx
@@ -68,7 +68,7 @@ export const WithIcons: Story = {
     onChange: () => {},
     label: "Search",
     placeholder: "Search...",
-    LeftIcon: <Search className="size-4" />,
+    leftIcon: <Search className="size-4" />,
     helper: "Type to search",
   },
 };
