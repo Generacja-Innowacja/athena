050eb2f45de4974d51b918f74d9e2fca6c350f79
kuba kesler <keslerkuba12@gmail.com>
2026-03-04 12:47:49 +0100

    Update ProgressBar.tsx
    Signed-off-by: kuba kesler <keslerkuba12@gmail.com>

--- src/components/ui/ProgressBar/ProgressBar.tsx
+++ src/components/ui/ProgressBar/ProgressBar.tsx
@@ -11,12 +11,12 @@ const progressBarVariants = cva(
         small: "h-2",
         regular: "h-4",
       },
-      variant: {
-        default: "",
-        info: "",
-        success: "",
-        warning: "",
-        error: "",
+  variant: {
+  default: "bg-slate-900",
+  info: "bg-blue-500",
+  success: "bg-green-500",
+  warning: "bg-yellow-500",
+  error: "bg-red-500",
       },
     },
     defaultVariants: {
