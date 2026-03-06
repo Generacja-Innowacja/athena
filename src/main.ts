1c78de94e67b93fb9f0b4b64af9951c4ac0b1da6
kuba kesler <keslerkuba12@gmail.com>
2026-03-06 10:53:08 +0100

    Update main.ts
    Signed-off-by: kuba kesler <keslerkuba12@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,3 @@ export { RadioGroup } from "./components/ui/RadioGroup/RadioGroup";
 export { RadioGroupItem } from "./components/ui/RadioGroup/RadioGroupItem/RadioGroupItem";
 export { Switch } from "./components/ui/Switch/Switch";
 export { Input } from "./components/ui/Input/Input";
-export { TextArea } from "./components/ui/TextArea/TextArea";
