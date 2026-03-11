ed5c2e37834f76ee698b2cb64bd9088822847b60
kuba kesler <keslerkuba12@gmail.com>
2026-03-11 10:54:16 +0100

    Update Input.tsx
    Signed-off-by: kuba kesler <keslerkuba12@gmail.com>

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -90,7 +90,7 @@ export function Input({
     {RightIcon}
     {suffix && <span className="text-[#80A2A9]">{suffix}</span>}
   </div>
-      
+    )}  
      {isError && errorText ? (
         <p className="text-sm text-gi-red">{errorText}</p>
       ) : (
